c4f5968bf25e947edf50a96353393d9547bae8fb
AltaJD <altair231002@gmail.com>
2024-03-10 22:00:06 +0800

    1. Determined the proportion of valid sensor values

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ def compress_sensors_data(df: pd.DataFrame) -> list[list[float]]:
 
 
 def get_compressed_data(df: pd.DataFrame, group_column: str) -> dict:
+    """ Returns the data in the format:
+    {"Posture Name": [float1, float2, ...]}
+    """
     result = {}
     grouped_df = df.groupby(group_column)
     for group_name, data_frame in grouped_df:
@@ -41,11 +44,41 @@ def get_compressed_data(df: pd.DataFrame, group_column: str) -> dict:
     return result
 
 
+def describe_sensors_values(df_original: pd.DataFrame) -> None:
+    """ Shows the percentage of valid values over all received values
+    Assumption: error values are considered to be outside of range [lr, hr]
+    Formula: valid_values/all_values*100 rounded to 2 digits after comma
+    """
+    def filtered_list(values: list[float]) -> list[float]:
+        """ Return only the values in range [lr, hr]"""
+        lr = 650
+        hr = 900
+        return [value for value in values if lr <= value <= hr]
+
+    def get_lengths(sensors_values: list[list[float]]) -> list[int]:
+        """ Return the lengths of the values accordingly
+        Index of the list represents sensor id
+        """
+        return [len(values) for values in sensors_values]
+
+    data: list[list[float]] = compress_sensors_data(df_original)
+    orig_data_lengths = get_lengths(sensors_values=data)
+    cleaned_data = [filtered_list(values) for values in data]
+    cleaned_data_lengths = get_lengths(sensors_values=cleaned_data)
+    sensor_id = 1
+    # Show the percentage of valid values
+    for original_length, cleaned_length in zip(orig_data_lengths, cleaned_data_lengths):
+        accuracy = round(cleaned_length/original_length*100, 2)
+        print(f"Sensor {sensor_id} has accuracy {accuracy}%")
+        sensor_id += 1
+
+
 if __name__ == '__main__':
     """ Extract and clean data  """
     file = "posture_data.csv"
     df = pd.read_csv(file, delimiter=",")
     data_cleaner.strip_columns(df)
+    describe_sensors_values(df)
     df = data_cleaner.exclude_errors(df)
 
     """ Process data """
